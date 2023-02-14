9cf53b5602674597fb08e15d24c2e8050ff1d607
CodeyBrodey <102107941+CodeyBrodey@users.noreply.github.com>
2023-02-13 22:01:19 -0300

    Finish filter button

--- src/components/filter/filter.js
+++ src/components/filter/filter.js
@@ -2,14 +2,19 @@ import './style.css';
 
 function Filter() {
     return(
-        <select name='filter' className="filter" placeholder="Filter">
-            <option value='' hidden selected disabled className='filter__option'>Filter by Region</option>
-            <option value='africa' className="filter__option">Africa</option>
-            <option value='america' className="filter__option">America</option>
-            <option value='asia' className="filter__option">Asia</option>
-            <option value='europe' className="filter__option">Europe</option>
-            <option value='oceania' className="filter__option">Oceania</option>
-        </select>
+        <button className='filter '>
+            <span filter__text>Filter by Region</span>
+            <svg className='filter__icon' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                <path d="M201.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 338.7 54.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z"/>
+            </svg>
+            <div className="filter__options filter__options--open">
+                <button className='filter__options__btns'>Africa</button>
+                <button className='filter__options__btns'>America</button>
+                <button className='filter__options__btns'>Asia</button>
+                <button className='filter__options__btns'>Europe</button>
+                <button className='filter__options__btns'>Oceania</button>
+            </div>
+        </button>
     )
 }
 
