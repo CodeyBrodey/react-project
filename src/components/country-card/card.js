9c790d6522194d0878790868ea3ded687f26566d
CodeyBrodey <102107941+CodeyBrodey@users.noreply.github.com>
2023-03-04 11:48:48 -0300

    Card styling finish

--- src/components/country-card/card.js
+++ src/components/country-card/card.js
@@ -5,8 +5,12 @@ import React, { useState, useEffect } from 'react';
 function CountryCard(){
     
 let countries;
-let [countryNames, setCountryNames] = useState('');
-let [countryFlags, setCountryFlags] = useState('');
+let [countryName, setCountryName] = useState('');
+let [countryFlag, setCountryFlag] = useState('');
+let [countryPopulation, setCountryPopulation] = useState('');
+let [countryRegion, setCountryRegion] = useState('');
+let [countryCapital, setCountryCapital] = useState('');
+
 
 
     async function getData() {
@@ -14,22 +18,30 @@ let [countryFlags, setCountryFlags] = useState('');
         const data = await res.json();
         countries = data; 
         countries.forEach((country) => {
-            setCountryNames(country.name.common);
-            setCountryFlags(country.flags.png)
+            setCountryName(country.name.common);
+            setCountryFlag(country.flags.png);
+            setCountryPopulation(country.population);
+            setCountryRegion(country.region);
+            setCountryCapital(country.capital);
         })
         console.log(countries)
     }
     
     useEffect(() => {
         getData();
-    }, []);
+    });
 
 
 
     return(
         <div className='card'>
-            <img src={countryFlags} alt="" className='card__img'/>
-            <h3>{countryNames}</h3>
+            <img src={countryFlag} alt="" className='card__img'/>
+            <div className="card__info">
+                <h3 className='card__h3'>{countryName}</h3>
+                <p className='card__population'>Population: <span className='card__population--result'>{countryPopulation}</span></p>
+                <p className='card__region'>Region: <span className='card__region--result'>{countryRegion}</span></p>
+                <p className='capital'>Capital: <span className='card__capital--result'>{countryCapital}</span></p>
+            </div>
         </div>
     )
 }
