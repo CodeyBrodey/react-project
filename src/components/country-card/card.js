b36feaec8add4537de2ac716e780874706adb671
CodeyBrodey <102107941+CodeyBrodey@users.noreply.github.com>
2023-03-11 12:20:43 -0300

    Started card click functionality

--- src/components/country-card/card.js
+++ src/components/country-card/card.js
@@ -5,16 +5,28 @@ import React, { useState, useEffect } from 'react';
 function CountryCard(props){
     const { name, flags, population, region, capital} = props.country;
 
-    function HandleCardClick(){
-        let [ myDiv, setMyDiv ] = useState('');
-        setMyDiv(
-            <div className='CountryCard__info'>
-            </div>
-        )
+    function HandleCardClick(event){
+        event.currentTarget.classList.add('card--open')
+        if(event.currentTarget.classList.contains('card--open')){
+            console.log('nice')
+        }
     }
+    function HandleBackButton(){
+        
+    }
+    
 
     return(
         <div className='card' onClick={HandleCardClick}>
+            <div className='card--open'>
+                <button className="card--open__button" onClick={HandleBackButton}>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className='card--open__button__icon'>
+                        <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l128 128c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.3 288 480 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-370.7 0 73.4-73.4c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-128 128z"/>
+                    </svg>
+                    Back
+                </button>
+                <img src={flags.png} alt="" className='card__img'/>
+            </div>
             <img src={flags.png} alt="" className='card__img'/>
             <div className="card__info">
                 <h3 className='card__h3'>{name.common}</h3>
